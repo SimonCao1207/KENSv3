9152a5a5cf164bbbb2bec93291ca06187b8d8b22
Hai Nam <hainampbc@gmail.com>
2022-10-15 03:28:54 +0900

    add TCP state enum, pass 2 test open and bind

--- app/kens/TCPAssignment.hpp
+++ app/kens/TCPAssignment.hpp
@@ -33,18 +33,31 @@ public:
 
 protected:
 
+  enum TCP_STATE {
+    TCP_CLOSED,
+    TCP_LISTEN,
+    TPC_SYN_RCVD,
+    TCP_ESTABLISHED,
+    TCP_CLOSE_WAIT,
+    TCP_LAST_ACK,
+    //----------//
+    TCP_SYN_SENT,
+    TCP_FIN_WAIT_1,
+    TCP_FIN_WAIT_2,
+    TCP_TIME_WAIT,
+  }; 
+
   // structs
   struct Address {
     int port;
     int ip;
-    
     Address(int port, int ip): port(port), ip(ip) {}
   };
 
   struct BufferRcv {
     int bytesRcvd;
     std::queue<uint8_t> bufferData;
-    BufferRcv() : bytesRcvd(0), bytesAck(0), bufferData(std::queue<uint8_t>()) {}
+    BufferRcv() : bytesRcvd(0), bufferData(std::queue<uint8_t>()) {}
   };
 
   struct BufferSnd {
@@ -56,23 +69,23 @@ protected:
   };
 
   struct Sucket {
-    Address localAddr;
-    Address remoteAddr;
+    // Address localAddr();
+    // Address remoteAddr();
     std::pair<int, int> pairKey;
     BufferRcv bufferRcv;
     BufferSnd bufferSnd;
-    uint8_t state;
+    TCP_STATE state;
     UUID syscall_id;
     uint32_t seqNum;
     uint32_t ackNum;
-
-    Sucket(std::pair<int, int> pairKey, uint8_t state): pairKey(pairKey), state(state) {}
+    Sucket() : state(TCP_CLOSED) {}
+    Sucket(std::pair<int, int> pairKey, TCP_STATE state): pairKey(pairKey), state(state) {}
   };
 
   // maps & set
-  std::unordered_map<std::pair<int, int>, std::pair<sockaddr, socklen_t>> processToAddrInfo; 
+  std::unordered_map<std::pair<int, int>, std::pair<sockaddr, socklen_t>> pairKeyToAddrInfo; 
+  std::unordered_map<std::pair<int, int>, Sucket> pairKeyToSucket;  
   std::unordered_set<std::pair<int, int>> pairKeySet;
-  
   std::unordered_set<std::pair<uint32_t, uint16_t>> bindedAddress; 
 
 
@@ -80,12 +93,13 @@ protected:
   virtual void systemCallback(UUID syscallUUID, int pid,
                               const SystemCallParameter &param) final;
   virtual void packetArrived(std::string fromModule, Packet &&packet) final;
-
   virtual int _syscall_socket(int pid) final;
 	virtual void syscall_socket(UUID syscallUUID, int pid, int type, int protocol) final;
   virtual int _syscall_bind( int sockfd, int pid, struct sockaddr *addr, socklen_t addrlen) final;
   virtual void syscall_bind(UUID syscallUUID, int pid, int sockfd, struct sockaddr *addr, socklen_t addrlen) final;
   virtual void syscall_getsockname(UUID syscallUUID, int pid, int sockfd, struct sockaddr *addr, socklen_t* addrlen) final;
+  virtual void syscall_close(UUID syscallUUID, int pid, int sockfd) final;
+  virtual void _sendPacket(Sucket sucket, uint8_t type) final;
 }; 
 
 class TCPAssignmentProvider {
